b3edf4b226bc4b0e2d05648cf2cd2b46b31731fb
EuniceJung <sy981012@naver.com>
2020-08-15 16:41:36 +0900

    test

---
+++ app/src/main/java/com/example/mytest/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.mytest;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.app.DatePickerDialog;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    DatePicker datePicker;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);//원래 상단바의 이름을 감춤
+
+        textView = findViewById(R.id.titleText);
+        Date current = Calendar.getInstance().getTime();
+        String date = new SimpleDateFormat("yyyy년 MM월", Locale.getDefault()).format(current);
+        textView.setText(date);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onClick(View v) {
+                LocalDate today =LocalDate.now();
+                DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this, listener, today.getYear(), today.getMonthValue()-1, today.getDayOfMonth());
+                datePickerDialog.getDatePicker().setCalendarViewShown(false);
+                datePickerDialog.show();
+            }
+        });
+
+    }
+
+    private DatePickerDialog.OnDateSetListener listener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            textView.setText(year + "년 " + (monthOfYear+1) + "월 ");
+        }
+    };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.month_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.app_bar_settings){
+            Toast.makeText(this, "설정눌림", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
