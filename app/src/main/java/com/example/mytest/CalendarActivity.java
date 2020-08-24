e55eaff784a0a7bdb50d6756be821be3adfddafa
EuniceJung <sy981012@naver.com>
2020-08-24 19:00:38 +0900

    설정버튼클릭시 넘어감

--- app/src/main/java/com/example/mytest/CalendarActivity.java
+++ app/src/main/java/com/example/mytest/CalendarActivity.java
@@ -526,7 +526,10 @@ public class CalendarActivity extends AppCompatActivity {
             selectym=yearNum+"-"+monthnumstr+"-";
             return true;
         } else if (R.id.tab5 == item.getItemId()) {
-            Toast.makeText(this, "앱빠뭘빠설정빠", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this, SettingsActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            return true;
         }
 
         return true;
